2323b095c2de1bc1a09a20da44af9ccc55ede010
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-11 23:29:00 +0100

    update

--- django_venue/urls.py
+++ django_venue/urls.py
@@ -14,3 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.contrib import admin
+from django.urls import path
+from  venue.views import get_bookings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', get_bookings, name='get_bookings'),
+    
+]
