2cce4d09cdcdec6712696b4caab29230826a582d
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-11 22:03:34 +0100

    create a table in admin site

--- venue/admin.py
+++ venue/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Hall, Apartment, Tenant, Tenancy, Accommodation, Accommodation, Payment, BusinessOwner
 
-# Register your models here.
+
+@admin.register(Hall)
+class HallAdmin(admin.ModelAdmin):
+    list_display = ('accommodation_id', 'weddings', 'birthdays', 'parties', 'baby_shower', 'corporate_events', 'christmas', 'lifestyle_photoshot')
+
+@admin.register(Apartment)
+class ApartmentAdmin(admin.ModelAdmin):
+    list_display = ('accommodation_id', 'check_in', 'check_out', 'guests', 'rooms', 'apartment_availability')
+
+@admin.register(Tenant)
+class TenantAdmin(admin.ModelAdmin):
+    list_display = ('tenant_id', 'first_name', 'last_name', 'email', 'mobile_phone')
+
+@admin.register(Tenancy)
+class TenancyAdmin(admin.ModelAdmin):
+    list_display = ('accommodation_id', 'tenant', 'check_in', 'check_out', 'sleeps', 'venue_type')
+
+@admin.register(Accommodation)
+class AccommodationAdmin(admin.ModelAdmin):
+    list_display = ('tenant', 'accommodation_id')
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('tenant_id', 'full_name', 'email', 'pay_with', 'order_total')
+
+@admin.register(BusinessOwner)
+class BusinessOwnerAdmin(admin.ModelAdmin):
+    list_display = ('accommodation_id', 'name', 'business_name', 'email', 'vat')
+    
--- venue/models.py
+++ venue/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Hall(models.Model):
+    accommodation_id = models.AutoField(primary_key=True)
+    weddings = models.BooleanField(default=False)
+    birthdays = models.BooleanField(default=False)
+    parties = models.BooleanField(default=False)
+    baby_shower = models.BooleanField(default=False)
+    corporate_events = models.BooleanField(default=False)
+    christmas = models.BooleanField(default=False)
+    lifestyle_photoshot = models.BooleanField(default=False)
+
+class Apartment(models.Model):
+    accommodation_id = models.AutoField(primary_key=True)
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+    guests = models.PositiveIntegerField()
+    rooms = models.PositiveIntegerField()
+    apartment_availability = models.CharField(max_length=20, choices=[("available", "Available"), ("unavailable", "Unavailable")])
+
+class Tenant(models.Model):
+    tenant_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    dob = models.DateField()
+    plus_18s = models.PositiveIntegerField()
+    under_18s = models.PositiveIntegerField()
+    email = models.EmailField()
+    mobile_phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    post_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+    billing_address = models.CharField(max_length=200)
+
+class Tenancy(models.Model):
+    accommodation_id = models.AutoField(primary_key=True)
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+    sleeps = models.PositiveIntegerField()
+    venue_type = models.CharField(max_length=100)
+
+class Accommodation(models.Model):
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    accommodation_id = models.AutoField(primary_key=True)
+
+class Payment(models.Model):
+    tenant_id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=200)
+    post_code = models.CharField(max_length=20)
+    email = models.EmailField()
+    contact_details = models.CharField(max_length=100)
+    billing_address = models.CharField(max_length=200)
+    PAYMENT_METHOD_CHOICES = [
+        ('credit_card', 'Credit Card'),
+        ('paypal', 'PayPal'),
+    ]
+    pay_with = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES)
+    add_new_card = models.BooleanField(default=False)
+    donate_to_charity = models.BooleanField(default=False)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2)
+
+class BusinessOwner(models.Model):
+    accommodation_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    business_name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    email = models.EmailField()
+    vat = models.CharField(max_length=20)
+
+    
