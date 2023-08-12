832ea592e62c5be512960aaae3772398e817450c
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-08-12 12:38:17 +0100

    add bookings

--- venue/views.py
+++ venue/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Hall, Apartment, Tenant, Tenancy, Accommodation, Payment, BusinessOwner
 
 # View for displaying bookings
@@ -15,4 +15,44 @@ def get_bookings(request):
 
 # View for adding bookings
 def add_bookings(request):
+    if request.method == 'POST':
+        # Retrieve data from the form
+        hall_accommodation_id = request.POST.get('hallAccommodationId')
+        weddings = request.POST.get('weddings') == 'on'
+        birthdays = request.POST.get('birthdays') == 'on'
+        parties = request.POST.get('parties') == 'on'
+        baby_shower = request.POST.get('babyShower') == 'on'
+        corporate_events = request.POST.get('corporateEvents') == 'on'
+        christmas = request.POST.get('christmas') == 'on'
+        lifestyle_photoshoot = request.POST.get('lifestylePhotoshoot') == 'on'
+        
+        apartment_accommodation_id = request.POST.get('apartmentAccommodationId')
+        check_in = request.POST.get('checkIn')
+        check_out = request.POST.get('checkOut')
+        guests = int(request.POST.get('guests'))
+        rooms = int(request.POST.get('rooms'))
+        apartment_availability = request.POST.get('apartmentAvailability')
+        
+        # Create a new Accommodation object and save it to the database
+        accommodation = Accommodation(
+            hall_accommodation_id=hall_accommodation_id,
+            weddings=weddings,
+            birthdays=birthdays,
+            parties=parties,
+            baby_shower=baby_shower,
+            corporate_events=corporate_events,
+            christmas=christmas,
+            lifestyle_photoshoot=lifestyle_photoshoot,
+            apartment_accommodation_id=apartment_accommodation_id,
+            check_in=check_in,
+            check_out=check_out,
+            guests=guests,
+            rooms=rooms,
+            apartment_availability=apartment_availability
+        )
+        accommodation.save()
+
+        # Redirect to a success page or another view
+        return redirect('bookings')  # 
+
     return render(request, "venue/add_bookings.html")
